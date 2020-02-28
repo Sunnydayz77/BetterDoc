db137c691316d9e7d1410961766512809c8522f5
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-02-27 20:09:05 -0500

    developed provider details page more

--- better-doc-project/src/components/ProviderDetails.jsx
+++ better-doc-project/src/components/ProviderDetails.jsx
@@ -11,23 +11,42 @@ const Docs = props => {
       <h4 className="name">{currentDoc.profile.first_name} {currentDoc.profile.middle_name} {currentDoc.profile.last_name}, {currentDoc.profile.title}</h4>
       <p>Bio: {currentDoc.profile.bio}</p>
 
+      {currentDoc.practices &&
+        <div className="location">
+        <h5>Practice Locations:</h5>
+          {currentDoc.practices.map(practice =>
+            <h5> {practice.visit_address.street}, {practice.visit_address.city}, {practice.visit_address.state} {practice.visit_address.zip} <br></br>
+            {/* Phone number: {practice.phones.number} */}
+            </h5>
+          )}
+        </div>
+      }
+        
+      {/* {currentDoc.practices &&
+        <div className="location">
+          {currentDoc.practices.map(practice =>
+            <h5>Accepting New Patients? {practice.accepts_new_patients}</h5>
+          )}
+        </div>
+      } */}
 
+      
       {currentDoc.insurances &&
         <div className="insurances">
           <h5>Insurances Accepted:</h5>
           {currentDoc.insurances.map(insurance =>
-          <h5>{insurance.insurance_provider.name}</h5>
+          <h5>{insurance.insurance_provider.name} {insurance.insurance_plan.name}</h5>
           )}
         </div>
       }
       
-      {currentDoc.insurances &&
-      <div>
-        {currentDoc.insurances.map(insurance =>
-        <h5>{insurance.insurance_plan.name}</h5>
-        )}
-      </div> 
-      }
+
+      <div className="specialties">
+      <h5>Specialties:</h5>
+        {currentDoc.specialties.map(specialty =>
+          <h5>{specialty.name}</h5>
+          )}
+      </div>
     </div>
   );
 };
--- better-doc-project/src/components/ProviderMiniInfo.jsx
+++ better-doc-project/src/components/ProviderMiniInfo.jsx
@@ -4,15 +4,16 @@ import ProviderDetails from './ProviderDetails'
 
 const ProviderMiniInfo = ({ docs }) => {
   function addDefaultSource(event) {
-    event.target.src = 'https://i.ya-webdesign.com/images/caring-clipart-primary-care-physician-15.png'
+  event.target.src = 'https://i.ya-webdesign.com/images/caring-clipart-primary-care-physician-15.png'
   }
   return (
   <>  
     {
       docs.length > 0 &&
   
-    (docs.map((doc, index) => {
-      return (
+        (docs.map((doc, index) => {
+      
+        return (
         <>
           <div key={index} className="doc-card">
             
@@ -22,11 +23,12 @@ const ProviderMiniInfo = ({ docs }) => {
             
             <h5>NPI: {doc.npi}</h5>
 
+            
             {doc.practices &&
               <div className="location">
-                <h5>Location:</h5>
+              <h5>Practice Locations: </h5>
                 {doc.practices.map(practice =>
-                  <h5>{practice.visit_address.city}</h5>
+                  <h5>{practice.visit_address.city}, {practice.visit_address.state} {practice.visit_address.zip}</h5>
                 )}
               </div>
             }
@@ -37,24 +39,7 @@ const ProviderMiniInfo = ({ docs }) => {
               <h5>{specialty.name}</h5>
               )}
             </div>
-{/* 
-            {doc.insurances &&
-            <div className="insurances">
-            <h5>Insurances Accepted:</h5>
-            {doc.insurances.map(insurance =>
-              <h5>{insurance.insurance_provider.name}</h5>
-              )}
-            </div>
-            }
-              
-            {doc.insurances &&
-            <div>
-            {doc.insurances.map(insurance =>
-              <h5>{insurance.insurance_plan.name}</h5>
-              )}
-            </div> 
-            } */}
-            
+        
             
             <Link to={`/ProviderContainer/details/${doc.uid}`}>
               <button>More Information</button>
@@ -72,16 +57,3 @@ const ProviderMiniInfo = ({ docs }) => {
   
 export default ProviderMiniInfo
 
-
-{/* 
-{docs.length >= 1 ?
-  (docs.map((character, index) => {
-      return(
-          <div key = {index}>
-              {character.name}
-          </div>
-      )
-  }))
-  : 
-  (< img src = 'https://cdn.dribbble.com/users/361263/screenshots/3051905/imperial_emblem.gif' alt = "loading spinner"/>)
-  } */}
